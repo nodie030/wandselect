4425591dde4cd426b40f2a9bfdc57622efef74a4
nodie030 <102212503+nodie030@users.noreply.github.com>
2022-06-11 01:55:44 +0800

    Add files via upload

---
+++ assets/js/footer.js
@@ -0,0 +1,7 @@
+footer='    <section>\
+<div class="footerMain">\
+    Vistors：50<br>\
+    © 2022 Ollivander ALL Rights Reserved\
+</div>\
+</section>';
+document.write(footer);
---
+++ assets/js/hearder.js
@@ -0,0 +1,17 @@
+hearder='    <section>   <!--導覽列-->\
+<nav id="nav">\
+    <div class="logo nav"><a href="index.html">奧利凡德</a></div>\
+    <div class="product nav"><a href="product.html" >商品介紹</a></div>\
+    <div class="aboutus nav"><a href="aboutus.html" >關於我們</a></div>\
+    <div class="search nav">\
+    <form action="">\
+        <input type="text" placeholder="Search" name="search" class="search1">\
+    </form>\
+</div>\
+<div class="car nav"><a href="login.html"><img src="assets/img/member.png" width="23" class="member_img"></a></div>\
+<div class="car nav" id="">\
+    <a href="shoppingcar.html"><img src="assets/img/car (1).png" width="32" class="car_img"></a>\
+</div>\
+</nav>\
+</section>';
+document.write(hearder);
